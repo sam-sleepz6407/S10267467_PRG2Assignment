ebbee79903c1c8457759bc327bce145c5a8a436a
ankylosaurus <cheehsiaoensamuela@gmail.com>
2025-01-19 19:32:09 +0800

    debugged classes

---
+++ PRG2_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿//features 1, 4, 7 & 8
+using PRG2_Assignment
+
+//feature 1
+Dictionary<string, Airline> airlinedict = new Dictionary<string, Airline>();
+loadairlines(airlinedict);
+foreach (KeyValuePair<string, Airline> kvp in airlinedict)
+{
+    Console.WriteLine(kvp.Key);
+    Console.WriteLine(kvp.Value.ToString());
+    Console.WriteLine();
+}
+
+//methods
+void loadairlines(Dictionary<string, Airline> airlinedict)
+{
+    using (StreamReader sr = new StreamReader("airlines.csv"))
+    {
+        string s = sr.ReadLine();
+        while ((s = sr.ReadLine()) != null)
+        {
+            string[] sdeets = s.Split(',');
+            string name = sdeets[0];
+            string code = sdeets[1];
+            Airline airline = new Airline(name, code);
+            airlinedict[name] = airline;
+        }
+    }
+}
---
+++ PRG2_Assignment/Terminal.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG2_Assignment
+{
+    internal class Airline
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Airline(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+        //public bool AddFlight(Flight f)
+        //{
+
+        //}
+        //public bool CalculateFees()
+        //{
+
+        //}
+        //public bool RemoveFlight(Flight f)
+        //{
+
+        //}
+        public override string ToString()
+        {
+            return $"Name: {Name}, Code: {Code}";
+        }
+    }
+    internal class Terminal
+    {
+        public string TerminalName { get; set; }
+        public Dictionary<string, Airline> Airlines { get; set; }
+        public Dictionary<string, Flight> Flights { get; set; }
+        public Dictionary<string, BoardingGate> BoardingGates { get; set; }
+        public Dictionary<string, double> GateFees { get; set; }
+        public Terminal(string terminalname, Dictionary<string, Airline> airlines, Dictionary<string, Flight> flights, Dictionary<string, BoardingGate> boardinggates, Dictionary<string, double> gatefees)
+        {
+            TerminalName = terminalname;
+            Airlines = airlines;
+            Flights = flights;
+            BoardingGates = boardinggates;
+            GateFees = gatefees;
+        }
+        //public bool AddAirline(Airline a)
+        //{
+
+        //}
+        //public bool AddBoardingGate(BoardingGate b)
+        //{
+
+        //}
+        //public Airline GetAirlineFromFlight(Flight)
+        //{
+
+        //}
+        //public void PrintAirlineFees()
+        //{
+
+        //}
+        public override string ToString()
+        {
+            return $"Terminal name: {TerminalName}, Airlines: {Airlines}, Flights: {Flights}, Boarding Gates: {BoardingGates}, Gate Fees: {GateFees}";
+        }
+    }
+    internal class BoardingGate
+    {
+        public string GateName { get; set; }
+        public bool SupportsCFFT { get; set; }
+        public bool SupportsDDJB { get; set; }
+        public bool SupportsLWTT { get; set; }
+        public Flight Flight { get; set; }
+        public BoardingGate(string gatename, bool supportcfft, bool supportDDJB, bool supportlwtt, Flight flight)
+        {
+            GateName = gatename;
+            SupportsCFFT = supportcfft;
+            SupportsDDJB = supportDDJB;
+            SupportsLWTT = supportlwtt;
+            Flight = flight;
+        }
+        //public double CalculateFees()
+        //{
+
+        //}
+        public override string ToString()
+        {
+            return $"Gate name: {GateName}, Supports CFFT: {SupportsCFFT}, Supports DDJB: {SupportsDDJB}, Supports LWTT: {SupportsLWTT}, Flight: {Flight}";
+        }
+    }
+    abstract class Flight
+    {
+        public string FlightNumber { get; set; }
+        public string Origin { get; set; }
+        public string Destination { get; set; }
+        public DateTime ExpectedTime { get; set; }
+        public string Status { get; set; }
+        public abstract double CalculateFees();
+        public override string ToString()
+        {
+            return $"Flight number: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, Expected Time: {ExpectedTime}, Status: {Status}";
+        }
+    }
+}
