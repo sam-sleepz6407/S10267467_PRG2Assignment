b758efa162c7418527a32d2edb7990b4e15949a0
Valeraeee <S10270376@connect.np.edu.sg>
2025-01-31 23:43:37 +0800

    Update LWTTFlight.cs

--- PRG2_Assignment/LWTTFlight.cs
+++ PRG2_Assignment/LWTTFlight.cs
@@ -13,26 +13,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PRG2_Assignment
+namespace PRG_2_Assignment
 {
     class LWTTFlight : Flight
     {
         public double RequestFee { get; set; }
         public string SpecialRequestCode { get; set; }
+        public LWTTFlight(string fn, string ori, string dest, DateTime et, string stat, string src) : base(fn, ori, dest, et, stat)
+        {
+            SpecialRequestCode = src;
+        }
         public double CalculateFees()
         {
-            double fee = 0;
-
-            if (SpecialRequestCode == "LWTT")
-            {
-                fee += 500;
-            }
+            double fee = base.CalculateFees();
+            fee += 300;
+            if (Origin == "SIN") fee += 800;
+            if (Destination == "SIN") fee += 500;
+            if (SpecialRequestCode == "LWTT") fee += 500;
             return fee;
         }
-        public LWTTFlight(string fn, string ori, string dest, DateTime et, string stat) : base(fn, ori, dest, et, stat) { }
         public override string ToString()
         {
-            return $"Flight Number: {FlightNumber}, Origin: {Origin}, Destination: {Destination}, Expected Time: {ExpectedTime:hh:mm tt}, Status: {Status}, Special Request Code: {SpecialRequestCode}";
+            return base.ToString() + $", Special Request Code: {SpecialRequestCode}";
         }
     }
 }
